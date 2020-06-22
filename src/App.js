fbea35724576e1614bee5ce8975e549d0a975a19
krishnakotak <krishnakotak2020@gmail.com>
2020-06-22 08:09:03 +0530

    Added scroll component and also modified code to load robots from API instead of file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import Scroll from './Scroll';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
@@ -9,9 +9,15 @@ class App extends Component {
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchField: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}));
 	}
 
 	onSearchFieldChange = (event) => {
@@ -19,16 +25,22 @@ class App extends Component {
 	}
 
 	render(){
-		const filteredRobots = robots.filter(robot => {
+		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
 		})
-		return(
-			<div className="tc">
-			<h2 className="f2">RoboFriends</h2>
-			<SearchBox onSearchChange={this.onSearchFieldChange}/>
-			<CardList robots={filteredRobots}/>
-			</div>
-			);
+		if (this.state.robots.length === 0){
+			return <h2>Loading...</h2>
+		}else {
+			return(
+				<div className="tc">
+				<h2 className="f2">RoboFriends</h2>
+				<SearchBox onSearchChange={this.onSearchFieldChange}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+				</div>
+				);
+		}
 	}
 	
 }
