a8ce12f85acb5bfa1573bedd6738417a93af5c6b
krishnakotak <krishnakotak2020@gmail.com>
2020-06-24 16:07:31 +0530

    added redux middleware to display logging

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './containers/App';
 import { searchRobots } from './reducers.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
